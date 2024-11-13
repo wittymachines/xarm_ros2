04f8a06c0bb969ce725ca1a71ae02dc702f619de
Johannes Pankert <johannes.pankert@witty-machines.com>
2024-11-13 11:07:47 +0100

    deactivate tests

--- uf_ros_lib/setup.py
+++ uf_ros_lib/setup.py
@@ -17,7 +17,6 @@ setup(
     maintainer_email='vinman.cub@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
