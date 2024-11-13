d2afa728411cf616dd8d24423fc2705f8517974e
Johannes Pankert <johannes.pankert@witty-machines.com>
2024-11-13 12:11:55 +0100

    fix more tests

--- uf_ros_lib/setup.py
+++ uf_ros_lib/setup.py
@@ -5,7 +5,7 @@ package_name = 'uf_ros_lib'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -17,6 +17,7 @@ setup(
     maintainer_email='vinman.cub@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
